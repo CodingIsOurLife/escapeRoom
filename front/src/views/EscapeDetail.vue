fb1fe1893eb6e75186cde21c464ca764d03e6c94
pipi <pkh9847@gmail.com>
2022-09-27 23:00:54 +0900

    chore: css변경

--- front/src/views/EscapeDetail.vue
+++ front/src/views/EscapeDetail.vue
@@ -3,15 +3,17 @@
     <v-card class="mx-auto" max-width="700">
       <div class="white--text align-end" height="200px">
         <Map />
-        <v-card-title>강남방탈출이름</v-card-title>
+        <v-card-title>넥스트 에디션 강남1호점</v-card-title>
       </div>
 
-      <v-card-subtitle class="pb-0"> 주소 : </v-card-subtitle>
+      <v-card-subtitle class="pb-0">
+        주소 : 서울 서초구 강남대로79길 59 B3층
+      </v-card-subtitle>
 
       <v-card-text class="text--primary">
-        <div>추천인원:</div>
+        <div>추천인원: 2-4</div>
 
-        <div>별점: 별별별별별</div>
+        <div>별점: ★★★★</div>
       </v-card-text>
 
       <Review />
