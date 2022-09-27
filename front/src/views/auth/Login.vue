4cf5c394a48ddc33fcd500086c0901c54f375fc8
pipi <pkh9847@gmail.com>
2022-09-27 23:03:08 +0900

    refactor: ui vuetify로 변경

--- front/src/views/auth/Login.vue
+++ front/src/views/auth/Login.vue
@@ -1,187 +1,94 @@
 <template>
-  <div class="register">
-    <h3>로그인</h3>
-    <!-- <form v-on:submit.prevent="loginSubmit"> -->
-    <div class="flex">
-      <ul class="container">
-        <li class="item center">이메일</li>
-        <li class="item">
-          <input
-            type="text"
-            v-model="email"
-            placeholder="이메일을 입력하세요."
-            required
-          />
-        </li>
-      </ul>
-      <ul class="container">
-        <li class="item center">비밀번호</li>
-        <li class="item">
-          <input
-            type="password"
-            v-model="password"
-            placeholder="비밀번호를 입력하세요."
-            required
-          />
-        </li>
-        <li class="item"></li>
-      </ul>
+  <div class="container">
+    <h2>Sign In</h2>
+    <form>
+      <v-text-field
+        v-model="email"
+        :error-messages="emailErrors"
+        label="E-mail"
+        required
+        @input="$v.email.$touch()"
+        @blur="$v.email.$touch()"
+      ></v-text-field>
+      <v-text-field
+        type="password"
+        v-model="password"
+        label="password"
+        :error-messages="passwordErrors"
+        required
+        @input="$v.password.$touch()"
+        @blur="$v.password.$touch()"
+      ></v-text-field>
 
-      <ul class="container">
-        <li class="item center"></li>
-        <li class="item">
-          <button @click="login({ email: email, password: password })">
-            로그인하기
-          </button>
-        </li>
-        <li class="item"></li>
-      </ul>
-    </div>
-    <!-- </form> -->
+      <div style="float: left">
+        <v-btn outlined @click="login({ email: email, password: password })">
+          SIGN IN
+        </v-btn>
+      </div>
+      <div style="float: right">
+        <v-btn outlined @click="clear"> clear </v-btn>
+      </div>
+    </form>
   </div>
 </template>
 
 <script>
 //import axios from "axios";
 import { mapActions } from "vuex";
+import { validationMixin } from "vuelidate";
+import { helpers, required, maxLength, email } from "vuelidate/lib/validators";
+
+const pwVaild = helpers.regex(
+  "pwVaild",
+  /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/
+);
+
 export default {
-  data() {
-    return {
-      email: "",
-      password: "",
-      // token: "",
-    };
+  mixins: [validationMixin],
+
+  validations: {
+    email: { required, email },
+    password: { required, maxLength: maxLength(10), pwVaild },
   },
+
+  data: () => ({
+    password: "",
+    email: "",
+  }),
+
+  computed: {
+    emailErrors() {
+      const errors = [];
+      if (!this.$v.email.$dirty) return errors;
+      !this.$v.email.email && errors.push("이메일 형식에 맞게 입력해주세요");
+      !this.$v.email.required && errors.push("이메일을 입력해주세요");
+      return errors;
+    },
+    passwordErrors() {
+      const errors = [];
+      if (!this.$v.password.$dirty) return errors;
+      !this.$v.password.pwVaild &&
+        errors.push("비밀번호 형식에 맞게 입력해주세요");
+      !this.$v.password.required && errors.push("비밀번호를 입력해주세요");
+      return errors;
+    },
+  },
+
   methods: {
+    // submit() {
+    //   this.$v.$touch();
+    // },
     ...mapActions(["login"]),
-
-    // loginSubmit: function () {
-    //   //event.preventDefault();
-
-    //   //console.log(this.uemail, this.pw, this.nickname);
-    //   let url = "member/login";
-    //   let saveData = {
-    //     email: this.uemail,
-    //     pw: this.pw,
-    //   };
-    //   try {
-    //     axios
-    //       .post(url, JSON.stringify(saveData), {
-    //         headers: {
-    //           "Content-Type": `application/json`,
-    //         },
-    //       })
-    //       .then((res) => {
-    //         if (res.status === 200) {
-    //           this.result = res.data;
-    //           console.log(this.result);
-    //           this.token = this.result.token;
-    //           this.$cookie.set("accesstoken", res.data.token, 1);
-    //           axios.defaults.headers.common["access token"] = res.data.token;
-    //           alert("어서오세요");
-    //           this.$router.push("/");
-    //         }
-    //       });
-    //   } catch (error) {
-    //     console.log(error);
-    //   }
+    clear() {
+      this.$v.$reset();
+      this.password = "";
+      this.email = "";
+    },
   },
 };
 </script>
 <style>
-* {
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
-}
-ul,
-li {
-  list-style: none;
-}
-a {
-  text-decoration: none;
-  color: inherit;
-}
-.register {
-  width: 550px;
-  margin: 200px auto 0;
-  padding: 15px 20px;
-  background: white;
-  color: #2b2e4a;
-  font-size: 14px;
-  text-align: left;
-  box-shadow: 1px 1px 5px rgba(0, 0, 0, 0.2);
-}
-.register h3 {
-  font-size: 20px;
-  margin-bottom: 20px;
-  text-align: center;
-}
-.register input {
-  width: 100%;
-  height: 40px;
-  outline: none;
-  padding: 10px;
-  border: 1px solid #707070;
-  transition: 0.3s;
-}
-.register input:valid,
-.register input:focus {
-  border: 1px solid #2b2e4a;
-}
-.register .center {
-  display: flex;
-  align-items: center;
-}
-.register .flex {
-  display: flex;
-  flex-direction: column;
-}
-.register .flex .container {
-  display: grid;
-  grid-template-columns: 1fr 3fr 1fr;
-  margin-bottom: 10px;
-}
-.register .flex .container .item:first-child {
-  margin-right: 10px;
-}
-.register .flex .container .item .idcheck {
-  height: 100%;
-  margin-left: 10px;
-  padding: 5px 15px;
-  background: #2b2e4a;
-  border: 1px solid #2b2e4a;
-  color: white;
-  font-size: 12px;
-  transition: 0.3s;
-}
-.register .flex .container .item .idcheck:hover {
-  background: white;
-  color: #2b2e4a;
-}
-.register .flex .container .item select {
-  height: 40px;
-  padding: 10px;
-  border: 1px solid #2b2e4a;
-}
-.register .submit {
-  width: 100%;
-  height: 40px;
-  color: white;
-  border: none;
-  border: 1px solid #2b2e4a;
-  background: #2b2e4a;
-  transition: 0.3s;
-}
-.register .flex .container:last-child {
-  margin: 0;
-}
-.register .submit:hover {
-  width: 100%;
-  height: 40px;
-  border: none;
-  border: 1px solid #2b2e4a;
-  color: #2b2e4a;
-  background: white;
+.container {
+  margin: 10% 30% 10% 30%;
 }
 </style>
--- front/src/views/auth/SignUp.vue
+++ front/src/views/auth/SignUp.vue
@@ -1,78 +1,49 @@
 <template>
-  <div class="register">
-    <h3>회원가입</h3>
-              <p v-if="errors.length">
-    
-    <ul>
-      <li v-for="error in errors" :key="error">{{ error }}</li>
-    </ul>
-  </p>
-    <form v-on:submit.prevent="submitForm">
-      <div class="flex">
-        <ul class="container">
+  <div class="container">
+    <h2>Sign UP</h2>
+    <form>
+      <v-text-field
+        v-model="nickname"
+        :error-messages="nicknameErrors"
+        :counter="5"
+        label="NickName"
+        required
+        @input="$v.nickname.$touch()"
+        @blur="$v.nickname.$touch()"
+      ></v-text-field>
 
-          <li class="item center">닉네임</li>
-          <li class="item">
-            <input
-              type="text"
-              name="nickname"
-              v-model="nickname"
-              placeholder="닉네임을 입력하세요."
-              autofocus
-              required
-            />
-          </li>
-          <li class="item">
-            <button class="idcheck" @click="checkNick">중복확인</button>
-          </li>
-          <li class="item"></li>
-        </ul>
-        <ul class="container">
-          <li class="item center">이메일</li>
-          <li class="item">
-            <input
-              type="text"
-              v-model="uemail"
-              placeholder="이메일을 입력하세요."
-              required
-            />
-          </li>
-          <li class="item">
-            <button class="idcheck">인증하기</button>
-          </li>
-        </ul>
-        <ul class="container">
-          <li class="item center">비밀번호</li>
-          <li class="item">
-            <input
-              type="password"
-              v-model="pw"
-              placeholder="비밀번호를 입력하세요."
-              required
-            />
-          </li>
-          <li class="item"></li>
-        </ul>
-        <ul class="container">
-          <li class="item center">비밀번호 확인</li>
-          <li class="item">
-            <input
-              type="password"
-              v-model="pwcheck"
-              placeholder="비밀번호를 입력하세요."
-              required
-            />
-          </li>
-          <li class="item"></li>
-        </ul>
+      <v-text-field
+        v-model="email"
+        :error-messages="emailErrors"
+        label="E-mail"
+        required
+        @input="$v.email.$touch()"
+        @blur="$v.email.$touch()"
+      ></v-text-field>
+      <v-text-field
+        type="password"
+        v-model="password"
+        label="Password"
+        :error-messages="passwordErrors"
+        required
+        @input="$v.password.$touch()"
+        @blur="$v.password.$touch()"
+      ></v-text-field>
+      <v-text-field
+        type="password"
+        v-model="pwcheck"
+        label="Password Check"
+        :error-messages="passwordckErrors"
+        required
+        @input="$v.pwcheck.$touch()"
+        @blur="$v.pwcheck.$touch()"
+      ></v-text-field>
 
-        <ul class="container">
-          <li class="item center"></li>
-          <li class="item">
-            <button class="submit" type="submit">가입하기</button>
-          </li>
-          <li class="item"></li>
-        </ul>
+      <div style="float: left">
+        <v-btn outlined @click="submitForm"> SIGN UP </v-btn>
+      </div>
+      <div style="float: right">
+        <v-btn outlined @click="clear"> clear </v-btn>
       </div>
     </form>
   </div>
@@ -80,189 +51,136 @@
 
 <script>
 import axios from "axios";
-// import { validateEmail } from "@/utils/validation";
+import { mapActions } from "vuex";
+import { validationMixin } from "vuelidate";
+import { helpers, required, maxLength, email } from "vuelidate/lib/validators";
+
+const pwVaild = helpers.regex(
+  "pwVaild",
+  /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/
+);
+const nickVaild = helpers.regex("nickVaild", /^[a-z0-9_-]{2,6}$/);
+
 export default {
-  data() {
-    return {
-      uemail: "",
-      pw: "",
-      nickname: "",
-      pwcheck:"",
-      errors:[],
-    };
+  mixins: [validationMixin],
+
+  validations: {
+    email: { required, email },
+    nickname: { required, nickVaild },
+    password: { required, maxLength: maxLength(10), pwVaild },
+    pwcheck: { required },
   },
-  // computed: {
-  //   isUserVaild() {
-  //     return validateEmail(this.uemail);
-  //   },
-  // },
+
+  data: () => ({
+    password: "",
+    email: "",
+    nickname: "",
+    pwcheck: "",
+  }),
+
+  computed: {
+    emailErrors() {
+      const errors = [];
+      if (!this.$v.email.$dirty) return errors;
+      !this.$v.email.email && errors.push("이메일 형식에 맞게 입력해주세요");
+      !this.$v.email.required && errors.push("이메일을 입력해주세요");
+      return errors;
+    },
+    passwordErrors() {
+      const errors = [];
+      if (!this.$v.password.$dirty) return errors;
+      !this.$v.password.pwVaild &&
+        errors.push("비밀번호 형식에 맞게 입력해주세요");
+      !this.$v.password.required && errors.push("비밀번호를 입력해주세요");
+      return errors;
+    },
+    nicknameErrors() {
+      const errors = [];
+      if (!this.$v.nickname.$dirty) return errors;
+      !this.$v.nickname.nickVaild &&
+        errors.push("닉네임 형식에 맞게 입력해주세요");
+      !this.$v.nickname.required && errors.push("2자리이상 6자리이하");
+      return errors;
+    },
+    passwordckErrors() {
+      const errors = [];
+      if (!this.$v.pwcheck.$dirty) return errors;
+
+      if (this.pwcheck !== this.password)
+        errors.push("비밀번호가 동일하지 않습니다.");
+
+      !this.$v.pwcheck.required && errors.push("비밀번호를 입력해주세요");
+      return errors;
+    },
+  },
+
   methods: {
-    submitForm: function (event) {
-      event.preventDefault();
-      this.errors = [];
-      if (!this.nickname) {
-        this.errors.push("닉네임은 필수입니다.");        
-      }
-      if (!this.pw != this.pwcheck) {
-        this.errors.push("비밀번호가 동일하지 않습니다");        
-      }
-      if (!this.uemail) {
-        this.errors.push("이메일은 필수입니다.");
-      } else if (!this.validEmail(this.uemail)) {
-        this.errors.push("이메일 형식을 확인하세요.");
-      }
-      if (!this.errors.length) return true;
+    // submit() {
+    //   this.$v.$touch();
+    // },
+    ...mapActions(["login"]),
+    clear() {
+      this.$v.$reset();
+      this.password = "";
+      this.email = "";
+    },
+  },
+  submitForm: function (event) {
+    event.preventDefault();
 
-      console.log(this.uemail, this.pw, this.nickname);
+    let url = "api/member/register";
+    let datas = {
+      nickname: this.nickname,
+      pw: this.pw,
+      email: this.uemail,
+    };
+    axios
+      .post(url, JSON.stringify(datas), {
+        headers: {
+          "Content-Type": `application/json`,
+        },
+      })
+      .then((res) => {
+        res.data = this.nickname;
+        if (res.status === 200) {
+          alert(this.nickname + "님 회원가입이 완료되었습니다.");
+          this.$router.push("/");
+        }
+      })
+      .catch(function (error) {
+        console.log(error);
+        console.log("연결실패");
+      });
+  },
 
-      let url = "api/member/register";
-      let datas = {
-        nickname: this.nickname,
-        pw: this.pw,
-        email: this.uemail,
-      };
-      axios
-        .post(url, JSON.stringify(datas), {
-          headers: {
-            "Content-Type": `application/json`,
-          },
-        })
-        .then((res) => {
-          res.data = this.nickname;
-          if (res.status === 200) {
-            alert(this.nickname + "님 회원가입이 완료되었습니다.");
-            this.$router.push("/");
-          }
-        })
-        .catch(function (error) {
-          console.log(error);
-          console.log("연결실패");
-        });
-    },
-    validEmail: function (email) {
-     //eslint-disable-next-line
-      var re =
-       /\S+@\S+\.\S+/;
-      return re.test(email);
-    },
-    checkNick: function () {
-      let url = "api/member/#";
-      let data = {
-        nickname: this.nickname,
-      };
-      axios
-        .post(url, JSON.stringify(data), {
-          headers: {
-            "Content-Type": `application/json`,
-          },
-        })
-        .then((res) => {
-          res.data = this.nickname;
-          if (res.status === 200) {
-            alert("중복된 아이디입니다");
-          }
-        })
-        .catch(function (error) {
-          console.log(error);
-        });
-    },
+  checkNick: function () {
+    let url = "api/member/#";
+    let data = {
+      nickname: this.nickname,
+    };
+    axios
+      .post(url, JSON.stringify(data), {
+        headers: {
+          "Content-Type": `application/json`,
+        },
+      })
+      .then((res) => {
+        res.data = this.nickname;
+        if (res.status === 200) {
+          alert("중복된 아이디입니다");
+        }
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
   },
 };
 </script>
 <style>
-* {
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
+.container {
+  margin: 10% 30% 30% 30%;
 }
-ul,
-li {
-  list-style: none;
-}
-a {
-  text-decoration: none;
-  color: inherit;
-}
-.register {
-  width: 550px;
-  margin: 200px auto 0;
-  padding: 15px 20px;
-  background: white;
-  color: #2b2e4a;
-  font-size: 14px;
-  text-align: left;
-  box-shadow: 1px 1px 5px rgba(0, 0, 0, 0.2);
-}
-.register h3 {
-  font-size: 20px;
-  margin-bottom: 20px;
-  text-align: center;
-}
-.register input {
-  width: 100%;
-  height: 40px;
-  outline: none;
-  padding: 10px;
-  border: 1px solid #707070;
-  transition: 0.3s;
-}
-.register input:valid,
-.register input:focus {
-  border: 1px solid #2b2e4a;
-}
-.register .center {
-  display: flex;
-  align-items: center;
-}
-.register .flex {
-  display: flex;
-  flex-direction: column;
-}
-.register .flex .container {
-  display: grid;
-  grid-template-columns: 1fr 3fr 1fr;
+.v-text-field__details {
   margin-bottom: 10px;
-}
-.register .flex .container .item:first-child {
-  margin-right: 10px;
-}
-.register .flex .container .item .idcheck {
-  height: 100%;
-  margin-left: 10px;
-  padding: 5px 15px;
-  background: #2b2e4a;
-  border: 1px solid #2b2e4a;
-  color: white;
-  font-size: 12px;
-  transition: 0.3s;
-}
-.register .flex .container .item .idcheck:hover {
-  background: white;
-  color: #2b2e4a;
-}
-.register .flex .container .item select {
-  height: 40px;
-  padding: 10px;
-  border: 1px solid #2b2e4a;
-}
-.register .submit {
-  width: 100%;
-  height: 40px;
-  color: white;
-  border: none;
-  border: 1px solid #2b2e4a;
-  background: #2b2e4a;
-  transition: 0.3s;
-}
-.register .flex .container:last-child {
-  margin: 0;
-}
-.register .submit:hover {
-  width: 100%;
-  height: 40px;
-  border: none;
-  border: 1px solid #2b2e4a;
-  color: #2b2e4a;
-  background: white;
 }
 </style>
